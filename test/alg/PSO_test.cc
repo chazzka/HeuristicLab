1ec077f3b90a612e04101b909b1a73402c878796
chazzka <adamo.ulrich@gmail.com>
2021-04-20 23:01:51 +0200

    PSO test fix

--- test/alg/PSO_test.cc
+++ test/alg/PSO_test.cc
@@ -1,18 +1,16 @@
 #include "gtest/gtest.h"
-#include "test/mock/utils_mock.cc" //every method returns 1.
+#include "test/mock/utils_mock.cc"            //every method returns 1.
 #include "test/mock/cec_20_test_func_mock.cc" //first dejong mock.
 #include "src/alg/PSO.cpp"
 
-
-Algorithm pso(1,1,1,1,1,1,1,1,1,-5.12,5.12);
+Algorithm pso(1, 1, 1, 1, 1, 1, 1, 1, 1, -5.12, 5.12);
 
 TEST(dimensionMove, movedCorrectly)
 {
     Particle p = {{1}, {1}, {1}};
     Particle gp = {{2}, {2}};
     pso.dimensionMove(p, gp, 0);
-    EXPECT_EQ(p.positionXi[0], 3);
-
+    ASSERT_EQ(p.positionXi[0], 3);
 }
 
 TEST(dimensionMove, isBackInDimension)
@@ -22,15 +20,10 @@ TEST(dimensionMove, isBackInDimension)
     pso.dimensionMove(p, gp, 0);
 
     ASSERT_LT(p.positionXi[0], 5.12);
-
 }
 
 TEST(run, isCorrectNumberOfFes)
 {
-    std::vector<result> res = pso.run(10,1,1,1);
+    std::vector<result> res = pso.run(10, 1, 1, 1);
     EXPECT_EQ(res.size(), 10);
-
 }
-
-
-
