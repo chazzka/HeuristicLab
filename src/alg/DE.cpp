241a21bbc0f334f076207cb6933bb2615fe8960b
chazzka <adamo.ulrich@gmail.com>
2021-04-30 21:41:55 +0200

    DE fix

--- src/alg/DE.cpp
+++ src/alg/DE.cpp
@@ -1,7 +1,16 @@
 #include <vector>
-#include "../utils/result.cpp"
-#include "../utils/utils.cpp"
-#include "../cec20_test_func.cpp"
+
+struct result
+{
+    int fez;
+    double cost;
+};
+
+struct Agent
+{
+    std::vector<double> position;
+    double cost;
+};
 
 class Algorithm
 {
@@ -9,13 +18,15 @@ private:
     int singleDimensionFes_, popSize_, dimension_, testFunction_;
     double CR_, F_, boundaryLow_, boundaryUp_;
 
-    struct Agent
+public:
+    Algorithm(int singleDimensionFes = 5000, int popSize = 50, int dimension = 0, int testFunction = 0,
+              double CR = 0.8, double F = 0.5, double boundaryLow = 0, double boundaryUp = 0)
+        : singleDimensionFes_(singleDimensionFes), popSize_(popSize), dimension_(dimension), testFunction_(testFunction),
+          CR_(CR), F_(F), boundaryLow_(boundaryLow), boundaryUp_(boundaryUp)
     {
-        std::vector<double> position;
-        double cost;
-    };
+    }
 
-    std::vector<Agent> get3unique(std::vector<Agent> populace, Agent jed)
+    std::vector<Agent> get3distinct(std::vector<Agent> populace, Agent jed)
     {
 
         int i = 0;
@@ -33,7 +44,7 @@ private:
 
         for (int i = 0; i < 3; i++)
         {
-            int ran = utils::generateRandomInt(0,populace.size());
+            int ran = utils::generateRandomInt(0, populace.size() - 1);
             result.push_back(populace.at(ran));
             populace.erase(populace.begin() + ran);
         }
@@ -41,7 +52,7 @@ private:
         return result;
     }
 
-    std::vector<Agent> get2unique(std::vector<Agent> populace, Agent jed)
+    std::vector<Agent> get2distinct(std::vector<Agent> populace, Agent jed)
     {
         int i = 0;
         for (Agent j : populace)
@@ -57,7 +68,7 @@ private:
         std::vector<Agent> result;
         for (int i = 0; i < 2; i++)
         {
-            int ran = utils::generateRandomInt(0,populace.size());
+            int ran = utils::generateRandomInt(0, populace.size() - 1);
             result.push_back(populace.at(ran));
             populace.erase(populace.begin() + ran);
         }
@@ -70,15 +81,10 @@ private:
         for (int i = 0; i < popSize_; i++)
         {
             std::vector<double> randomPosition = utils::generateRandomRange(dimension_, boundaryLow_, boundaryUp_);
-            //Initialize the particle's position with a uniformly distributed random vector xi ~ U(BOUNDARY_LOW, BOUNDARY_UP)
-            //TODO initial velocity 0?
-            //Initialize the particle's velocity: vi ~ U(-|BOUNDARY_UP-BOUNDARY_LOW|, |BOUNDARY_UP-BOUNDARY_LOW|), maybe 0 would be better
-            //Initialize the particle's best known position to its initial position: pi ← xi
             Agent a = {randomPosition, 0};
             cec20_test_func(a.position.data(), &a.cost, dimension_, 1, testFunction_);
             if (a.cost < gCost)
             {
-                //update the swarm's best known position: g ← pi
                 leadingPosG = a.position;
                 gCost = a.cost;
             }
@@ -86,24 +92,35 @@ private:
         }
     }
 
-    void backToBoundariesRandom(double yi, std::vector<double> &y)
+    double backToBoundariesMirror(double yi)
+    {
+        if (yi < boundaryLow_)
+        {
+            yi = boundaryLow_ + (boundaryLow_ - yi);
+        }
+        if (yi > boundaryUp_)
+        {
+            yi = boundaryUp_ + (boundaryUp_ - yi);
+        }
+        return yi;
+    }
+
+    double backToBoundariesRandom(double yi)
     {
         if (boundaryLow_ > yi || yi > boundaryUp_)
         {
-            y.push_back(utils::generateRandomDouble(boundaryLow_, boundaryUp_));
+            return utils::generateRandomDouble(boundaryLow_, boundaryUp_);
         }
         else
         {
-            y.push_back(yi);
+            return yi;
         }
     }
 
-public:
-    Algorithm(int singleDimensionFes = 5000, int popSize = 50, int dimension = 0, int testFunction = 0,
-              double CR = 0.8, double F = 0.5, double boundaryLow = 0, double boundaryUp = 0)
-        : singleDimensionFes_(singleDimensionFes), popSize_(popSize), dimension_(dimension), testFunction_(testFunction),
-          CR_(CR), F_(F), boundaryLow_(boundaryLow), boundaryUp_(boundaryUp)
+    double dimensionMove(double ai, double bi, double ci)
     {
+        double yi = ai + F_ * (bi - ci);
+        return backToBoundariesMirror(yi);
     }
 
     std::vector<result> run(int dimension, int testFunction, double boundaryLow, double boundaryUp)
@@ -124,28 +141,25 @@ public:
 
         initPopulation(population, gPosition, gCost);
 
-        while (iterations--)
+        for (int i = 0; i < iterations; i++)
         {
-            std::vector<Agent> uiPopulace;
-            //For each agent x in the population do:
             for (Agent x : population)
             {
-                //Pick three unique agents
-                std::vector<Agent> twoUnique = get2unique(population, x);
+                std::vector<Agent> twoDistinct = get2distinct(population, x);
+
                 //Pick a random index R
-                double R = utils::generateRandomDouble(1, dimension_);
+                int R = utils::generateRandomInt(1, dimension_);
                 //Compute the agent's potentially new position y
                 std::vector<double> y;
+
                 for (int d = 0; d < dimension_; d++)
                 {
                     //pick a uniformly distributed random number ri
-                    int r = utils::generateRandomInt(1, dimension_);
+                    double r = utils::generateRandomDouble(0, 1);
                     if (r < CR_ || d == R)
                     {
                         //verze BEST/1/bin
-                        double yi = gPosition[d] + F_ * (twoUnique[0].position[d] - twoUnique[1].position[d]);
-                        //out of dimension check - TODO, udělej mirroring nebo reflection
-                        backToBoundariesRandom(yi, y);
+                        y.push_back(dimensionMove(gPosition[d], twoDistinct[0].position[d], twoDistinct[1].position[d]));
                     }
                     // otherwise set yi = xi
                     else
@@ -153,6 +167,7 @@ public:
                         y.push_back(x.position[d]);
                     }
                 }
+
                 double yCost = 0;
                 cec20_test_func(y.data(), &yCost, dimension, 1, testFunction);
                 fezCounter++;
@@ -169,10 +184,7 @@ public:
                     }
                 }
 
-                result res;
-                res.cost = gCost;
-                res.fez = fezCounter;
-                bestResults.push_back(res);
+                bestResults.push_back({fezCounter, gCost});
             }
         }
         return bestResults;
